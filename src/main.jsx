717c26f77b3e0f81574a051f100f3b619ae09bef
jojoh45 <jojoh45@morgan.edu>
2025-02-16 20:18:36 -0500

    Updated dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
-// import { BrowserRouter } from 'react-router';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router';
 import './index.css';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
