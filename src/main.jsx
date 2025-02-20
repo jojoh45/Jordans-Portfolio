baee9f58848da4a9e9e7f092cb76289214d971dc
jojoh45 <jojoh45@morgan.edu>
2025-02-20 18:20:22 -0500

    changed back to browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router';
+import { BrowserRouter } from 'react-router';
 import './index.css';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
 )
