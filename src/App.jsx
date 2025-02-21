84b6888d86bf37f041bdb4f4792e209ef4476780
jojoh45 <jojoh45@morgan.edu>
2025-02-20 22:13:05 -0500

    little update

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ function App() {
           <Experience />
           <Projects />
           <Footer/>
+          <Analytics />
         </div>
       </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,9 @@ import { BrowserRouter } from 'react-router';
 import './index.css';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
-    <Analytics />
   </BrowserRouter>,
 )
